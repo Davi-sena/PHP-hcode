63eaecbfcdafe78d95c13c5eabfd4fb59c550f64
davisena_js <80988584+davisena2021@users.noreply.github.com>
2022-01-22 07:02:16 -0300

    Classe animal adicionada

--- Class/Sql.php
+++ Class/Sql.php
@@ -5,31 +5,31 @@ class Sql extends PDO{
     private $conn;
 
     public function __construct(){
-        $this->conn = new PDO("mysql:host=localhost; dbname=ong","root","root");
+
+        $this->conn = new PDO("mysql:host=localhost; dbname=ong", "root", "root");
     }
 
-    public function execQuery($rawQuery,$params = array()){
+    private function setParams($statement, $parameters = array()){
+        foreach ($parameters as $key => $value) {
+            $this->setParam($statement, $key, $value);
+        }
+    }
+
+    private function setParam($statement, $key, $value){
+
+        $statement->bindParam($key, $value);
+    }
+
+    public function execQuery($rawQuery, $params = array()){
         $stmt = $this->conn->prepare($rawQuery);
-        $this->setParams($rawQuery,$params);
+        $this->setParams($stmt, $params);
         $stmt->execute();
         return $stmt;
     }
 
-    public function setParams($statment,$parameters = array()){
-        foreach($parameters as $key => $value){
-            $this->setParam($statment,$key,$value);
-        }
-    }
-    
-    public function setParam($statment,$key,$value){
-        $statment->bind_param($key,$value);
-    }
-
-    public function select($rawQuery, $params = array()):array{
+    public function select($rawQuery, $params = array()): array{
 
         $stmt = $this->execQuery($rawQuery, $params);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
-
--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 require_once("config.php");
 
-$usu = new Usuario();
-$usu->loadByCpf(1);
-echo $usu;
+// $usu = new Usuario();
+// $usu->loadByCpf(1);
+// echo $usu."<br>";
 
+$animal = new Animal();
+$animal->loadByChip(1);
+echo $animal;
 ?>
