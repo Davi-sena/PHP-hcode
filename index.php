0cb030a74c7cbd0897aaa93e865c996c1adf8d02
davisena_js <80988584+davisena2021@users.noreply.github.com>
2022-01-22 07:15:45 -0300

    Classes criadas e organizadas

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
 require_once("config.php");
-
-// $usu = new Usuario();
-// $usu->loadByCpf(1);
-// echo $usu."<br>";
-
-$animal = new Animal();
-$animal->loadByChip(1);
-echo $animal;
-?>
